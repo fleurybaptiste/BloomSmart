df6c0efb272b2d262f8fa095207ce9f0595f8bd5
fleury <fleury.baptiste.oclock@gmail.com>
2024-01-21 00:51:11 +0100

    Adding logique to hours minuts ands days

---
+++ BloomSmart/src/components/spinner.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import '../styles/spinner.css'; // Assurez-vous que le chemin est correct
+
+const Spinner = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Spinner;
--- BloomSmart/src/pages/vmc.jsx
+++ BloomSmart/src/pages/vmc.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import '../styles/graph.css';
 import axios from 'axios';
+import Spinner from '../components/spinner';
 import { Line } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
@@ -27,7 +28,7 @@ const options = {
         },
         title: {
             display: true,
-            text: 'Graphique',
+            text: 'Salon',
         },
     },
     scales: {
@@ -41,7 +42,7 @@ const options = {
         x: {
             // Ajustements pour l'axe X
             ticks: {
-                maxTicksLimit: 20,
+                maxTicksLimit: 200,
                 autoSkip: true,
                 maxRotation: 45,
                 minRotation: 45,
@@ -50,28 +51,42 @@ const options = {
     },
 };
 
+const fetchDataLastHour = () => axios.get('http://192.168.1.20:3000/api/data/last-hour');
+const fetchDataLastDay = () => axios.get('http://192.168.1.20:3000/api/data/last-day');
+const fetchDataLastMonth = () => axios.get('http://192.168.1.20:3000/api/data/last-month');
+
 function App() {
     const [sensorData, setSensorData] = useState({ temperatures: [], humidities: [], labels: [] });
-
-    const fetchData = () => {
-        axios
-            .get('http://192.168.1.20:3000/api/data')
-            .then((response) => {
-                const data = response.data;
-                const temperatures = data.map((item) => item.temperature);
-                const humidities = data.map((item) => item.humidity);
-                const labels = data.map((item) => new Date(item.createdAt).toLocaleTimeString());
-                setSensorData({ temperatures, humidities, labels });
-            })
-            .catch((error) => console.error('Erreur lors de la récupération des données:', error));
-    };
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentPeriod, setCurrentPeriod] = useState('hour');
 
     useEffect(() => {
-        fetchData(); // Charger les données initialement
-        const interval = setInterval(fetchData, 5000); // Rafraîchir toutes les 5 secondes
+        setIsLoading(true); // Débute le chargement
 
-        return () => clearInterval(interval); // Nettoyer l'intervalle quand le composant est démonté
-    }, []);
+        let fetchFunction = fetchDataLastHour; // Valeur par défaut
+        if (currentPeriod === 'day') {
+            fetchFunction = fetchDataLastDay;
+        } else if (currentPeriod === 'month') {
+            fetchFunction = fetchDataLastMonth;
+        }
+
+        fetchFunction()
+            .then((response) => {
+                const data = response.data;
+                localStorage.setItem('sensorData', JSON.stringify(data));
+                setSensorData({
+                    temperatures: data.map((item) => item.temperature),
+                    humidities: data.map((item) => item.humidity),
+                    labels: data.map((item) => new Date(item.createdAt).toLocaleTimeString()),
+                });
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la récupération des données:', error);
+            })
+            .finally(() => {
+                setIsLoading(false); // Termine le chargement
+            });
+    }, [currentPeriod]);
 
     const lineData = {
         labels: sensorData.labels,
@@ -81,21 +96,32 @@ function App() {
                 data: sensorData.humidities,
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                borderWidth: 1,
             },
             {
                 label: 'Température',
                 data: sensorData.temperatures,
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 1,
             },
         ],
     };
 
     return (
         <>
-            <div className="chart-container">
-                <Line options={options} data={lineData} />
+            <div>
+                <button onClick={() => setCurrentPeriod('hour')}>Dernière Heure</button>
+                <button onClick={() => setCurrentPeriod('day')}>Dernière Journée</button>
+                <button onClick={() => setCurrentPeriod('month')}>Dernier Mois</button>
             </div>
+            {isLoading ? (
+                <Spinner /> // Affiche le spinner pendant le chargement
+            ) : (
+                <div className="chart-container">
+                    <Line options={options} data={lineData} />
+                </div>
+            )}
         </>
     );
 }
